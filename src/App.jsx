89123193f12e5c0e809634e73ddef1cfffed703d
Olena <newsroommy@gmail.com>
2023-01-15 11:39:32 +0200

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,6 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-        // username="Jacques Gluke"
-        // tag="jgluke"
-        // location="Ocho Rios, Jamaica"
-        // avatar="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
-        // stats="5603 4827 1308"
       />
     </>
   );
